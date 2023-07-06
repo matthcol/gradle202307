2081389c040be9ec3bd70c0c23eaf3c5dc44aef5
matthcol <matthias.colin@gmail.com>
2023-07-06 12:40:04 +0200

    end morning 1

--- hello/src/test/java/org/example/dto/MovieTest.java
+++ hello/src/test/java/org/example/dto/MovieTest.java
@@ -1,10 +1,21 @@
 package org.example.dto;
 
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieTest {
+    static Validator validator;
+
+    @BeforeAll
+    static void initValidator(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 
     @Test
     void testConstructorAllArgs() {
@@ -39,5 +50,22 @@ class MovieTest {
                 () -> assertEquals(year, movie.getYear(), "year"),
                 () -> assertEquals(duration, movie.getDuration(), "duration")
         );
+    }
+
+    @Test
+    void testTitleNotValid() {
+        String title = "";
+        short year = 2017;
+        var movie = Movie.builder()
+                .title(title)
+                .year(year)
+                .build();
+        var constraintViolations = validator.validate(movie);
+        assertEquals(1, constraintViolations.size(), "title not blank");
+    }
+
+    @Test
+    void testYearNotValid() {
+
     }
 }
